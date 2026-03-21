03ddbac8612da6f82559b7524f5e4680b2a523d9
SiamAhmad1260 <siam.ahmad.d1260@gmail.com>
2026-03-22 00:23:15 +0600

    change

--- 1.go
+++ 1.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	Challenge1()
+	Challenge_1_Switches()
 }
 
 func Saying_Hello() {
@@ -16,7 +16,7 @@ func Saying_Hello() {
 	fmt.Printf("Hello, %s, nice to meet you!", name)
 }
 
-func Challenge1() {
+func Challenge_1() {
 	var name string
 	fmt.Print("What is your name? ")
 	fmt.Scan(&name)
@@ -26,7 +26,7 @@ func Challenge1() {
 	} else if greeting == 1 {
 		fmt.Printf("Greetings, %s", name)
 	} else if greeting == 2 {
-		fmt.Printf("How's it going, %s", name)
+		fmt.Printf("How's it going %s?", name)
 	} else if greeting == 3 {
 		fmt.Printf("Meow meow %s", name)
 	} else if greeting == 4 {
@@ -35,5 +35,42 @@ func Challenge1() {
 		fmt.Print("Ja, Ja, Ja, Jaann!")
 	} else {
 		fmt.Print("I miss you!")
+	}
+}
+
+func Challenge_1_Switches() {
+	var name string
+	fmt.Print("What is your name? ")
+	fmt.Scan(&name)
+	greeting := rand.Intn(7)
+	switch {
+	case greeting == 0:
+		{
+			fmt.Printf("Hello, %s", name)
+		}
+	case greeting == 1:
+		{
+			fmt.Printf("Greetings, %s", name)
+		}
+	case greeting == 2:
+		{
+			fmt.Printf("How's it going %s?", name)
+		}
+	case greeting == 3:
+		{
+			fmt.Printf("Assalamu Alaikum, %s", name)
+		}
+	case greeting == 4:
+		{
+			fmt.Printf("meow meow, %s", name)
+		}
+	case greeting == 5:
+		{
+			fmt.Printf("Assalamu alaikum, ...")
+		}
+	case greeting == 6:
+		{
+			fmt.Printf("How are you, ...")
+		}
 	}
 }
