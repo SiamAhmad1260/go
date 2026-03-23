4d0c30800b6e8efcf5fc2bc207c001d19e528d3e
SiamAhmad1260 <siam.ahmad.d1260@gmail.com>
2026-03-23 22:38:25 +0600

    Just take it

---
+++ 2.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	Challenge_1()
+}
+
+func CountNumberOfCharactors() {
+	var str string
+	fmt.Print("what is the string input? ")
+	fmt.Scan(&str)
+	fmt.Printf("%s has %d charactors.", str, utf8.RuneCountInString(str))
+}
+
+func Challenge_1() {
+	var str string
+	fmt.Print("What is the string? ")
+	fmt.Scan(&str)
+	if str == "" {
+		panic("must have a proper input!")
+	} else {
+		fmt.Printf("%s contains %d number of charactors.", str, utf8.RuneCountInString(str))
+	}
+}
